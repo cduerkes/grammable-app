090006ab986c8fdcf7ec1c411a37563f22602375
Christopher Duerkes <christopher.duerkes@gmail.com>
2017-08-03 18:33:31 +0000

    Add ability to edit and update grams.

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -1,6 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe GramsController, type: :controller do
+  describe "grams#update action" do 
+    it "should allow users to successfully update grams" do
+      gram = FactoryGirl.create(:gram, message: "Initial value")
+      patch :update, params: { id: gram.id, gram: { message: "Changed" } }
+      expect(response).to redirect_to root_path
+      gram.reload
+      expect(gram.message).to eq "Changed"
+    end
+  end
+
+  describe "grams#update action" do
+    it "should return a 404 error if the gram is not found" do 
+      patch :update, params: { id: "YOLOSWAG", gram: { message: "Changed" } }
+      expect(response).to have_http_status(:not_found)
+    end
+  end
+
+  describe "grams#update action" do 
+    it "should render the edit form with an http status of unprocessable_entity" do
+      gram = FactoryGirl.create(:gram, message: "Initial value")
+      patch :update, params: { id: gram.id, gram: { message: '' } }
+      expect(response).to have_http_status(:unprocessable_entity)
+      gram.reload
+      expect(gram.message).to eq "Initial value"
+    end
+  end
+
   describe "grams#edit action" do
     it "should successfully show the edit form if the gram is found" do 
       gram = FactoryGirl.create(:gram)
